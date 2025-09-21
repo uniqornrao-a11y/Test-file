9eca6b2b252336b33318878340e9f96b925b0e83
uniqornrao-a11y <uniqornrao@gmail.com>
2025-09-21 14:03:04 -0700

    Update game.py

--- game.py
+++ game.py
@@ -4,4 +4,13 @@ choices = ["rock", "paper", "scissors"]
 device_pick = random.choice(choices)
 
 while True:
- player_pick = 
+    playerpick=str(input("Choose from rock, paper, and scissors:  "))
+    if playerpick in choices:
+        if playerpick==device_pick:
+            print("You are tied with the computer. ")
+        elif playerpick=="rock" and device_pick=="scissors" or playerpick=="paper" and device_pick=="rock" or playerpick=="scissors" and device_pick=="paper":
+            print("You win! :)")
+        elif  playerpick=="scissors" and device_pick=="rock" or playerpick=="rock" and device_pick=="paper" or playerpick=="paper" and device_pick=="scissors":  
+            print("You lost. :(")
+    else:
+        print(f"{playerpick} not recognized.")
